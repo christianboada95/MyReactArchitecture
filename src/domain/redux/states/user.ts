c341754bc53d345602b58296225239bd9dec2a3d
Cristian Boada <christian_dbp95@hotmail.com>
2023-08-14 22:32:33 -0500

    first commit

---
+++ src/domain/redux/states/user.ts
@@ -0,0 +1,10 @@
+import { User } from "../../models/user.model";
+//import { createSlice } from '@reduxjs/toolkit'
+
+export const UserEmptyState: User = {
+
+}
+
+// export const userSlice = createSlice({
+    
+// })
