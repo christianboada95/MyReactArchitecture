ca21c3f3d24a800a465a8377f518f25d5b9ec8ab
Cristian Boada <christian_dbp95@hotmail.com>
2023-08-15 23:27:12 -0500

    add some folders and reorganice project files

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './presentation/App.tsx'
+import App from './presentation/App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
